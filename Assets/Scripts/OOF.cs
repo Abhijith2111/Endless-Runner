3c63fdbc3edd131fd8b00dde6e753edee9c47659
LazyTallIndian <77723138+Abhijith2111@users.noreply.github.com>
2025-04-03 19:06:21 +0200

    Revamped the camera

--- Assets/Scripts/OOF.cs
+++ Assets/Scripts/OOF.cs
@@ -1,10 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
 public class OOF : MonoBehaviour
 {
     [SerializeField] GameObject Player1;
+    [SerializeField] GameObject CameraMain;
+    [SerializeField] GameObject Death;
+
     void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(CollisionEnd());
+    }
+
+    IEnumerator CollisionEnd()
+    {
         Player1.GetComponent<PlayerMovment>().enabled = false;
+        CameraMain.GetComponent<Animator>().Play("Hit The wall Cam");
+        yield return new WaitForSeconds(3);
+        Death.SetActive(true);
     }
 }
